9bda87394d39d1cd772cda5dfc8db2ed1ae18ee8
Saten Vardazaryan <sattenikvardazaryan@gmail.com>
2025-11-16 20:35:13 +0400

    Initial commit without node_modules

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -1,7 +1,7 @@
 const { browser } = require('@wdio/globals')
 
 module.exports = class Page {
-    open(path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
-    }
+    open () {
+        return browser.url('https://www.saucedemo.com/');
+      }
 }
